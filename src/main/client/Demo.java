d6eeb0a11dde1e1be699edf9f637126834888430
Willow Sapphire <willowsapphire@protonmail.com>
2023-11-28 13:22:50 -0500

    added empty demo

---
+++ src/main/client/Demo.java
@@ -0,0 +1,20 @@
+package client;
+
+/**
+ * A demonstration of the ArrayMap class.
+ * 
+ * @author
+ * @version
+ */
+public class Demo
+{
+    /**
+     * Runs the demo.
+     * 
+     * @param args unused.
+     */
+    public static void main(String[] args)
+    {
+        // put your demo here.
+    }
+}
